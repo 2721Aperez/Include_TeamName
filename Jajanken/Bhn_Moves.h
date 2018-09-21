9be049d36700e25ae1bbbde608bae6fa1f741b41
Alejandro Perez <alex2721ap@gmail.com>
2018-09-20 19:52:40 -0700

    Added the moveset to games

---
+++ Jajanken/Bhn_Moves.h
@@ -0,0 +1,8 @@
+#ifndef BHN_MOVES_H_
+#define BHN_MOVES_H_
+
+enum class Bhn_Moves
+{
+    BEAR, HUNTER, NINJA,
+};
+#endif
---
+++ Jajanken/Rps_Moves.h
@@ -0,0 +1,9 @@
+#ifndef RPS_MOVES_H_
+#define RPS_MOVES_H_
+
+enum class Rps_Moves
+{
+    ROCK, PAPER, SCISSORS,
+};
+
+#endif
