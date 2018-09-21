0da6bc9ae97c5b82b3f362ef51a0979fcbfe0635
Alejandro Perez <alex2721ap@gmail.com>
2018-09-20 20:31:22 -0700

    Added gameresults.h

---
+++ Jajanken/GameResults.h
@@ -0,0 +1,27 @@
+#ifndef GAMERESULT_H_
+#define GAMERESULT_H_
+
+class GameResults
+{
+private:
+    int player_wins, player_loss, comp_wins, comp_loss, ties;
+public:
+    void setPlayerWins(int);
+    void setPlayerLoss(int);
+
+    void setCompWins(int);
+    void setCompLoss(int);
+
+    void displayResults();
+    void displayRounds();
+
+    void getPlayerWins() const;
+    void getPlayerLoss() const;
+
+    void getCompWins() const;
+    void getCompLoss() const;
+
+    void getTies() const;
+
+};
+#endif
