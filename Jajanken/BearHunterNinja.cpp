7290a2954ab56b27ae488b501664497c2a6b5dda
Alejandro Perez <alex2721ap@gmail.com>
2018-09-23 13:55:22 -0700

    Mostly impplemented BHN

--- Jajanken/BearHunterNinja.cpp
+++ Jajanken/BearHunterNinja.cpp
@@ -1,2 +1,30 @@
-#include "BearHunterNinja.h"
+#include "BearHunterNinja.h" 
 
+void BearHunterNinja::displayRules()
+{
+    std::cout << "Hunter Shoots Bear" << std::endl;
+    std::cout << "Bear Eats Ninja" << std::endl;
+    std::cout << "Ninja Kills Hunter" << std::endl;
+}
+
+void BearHunterNinja::setPlayerMove(Bhn_Moves player){player_move = player;}
+
+Bhn_Moves BearHunterNinja::genCompMove() 
+{
+    Bhn_Moves move = static_cast<Bhn_Moves>(rand() % 3); 
+    return move;
+}
+
+void BearHunterNinja::setComputerMove(Bhn_Moves comp)
+{
+    comp = genCompMove();
+    computer_move = comp;;
+}
+
+void BearHunterNinja::displayMove()
+{
+
+}
+
+Bhn_Moves BearHunterNinja::getPlayerMove() const {return player_move;}
+Bhn_Moves BearHunterNinja::getComputerMove() const {return computer_move;}
