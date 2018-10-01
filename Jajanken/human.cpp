f1203aca291de37209a0a267f4b3823f24bb6ff7
Alejandro Perez <alex2721ap@gmail.com>
2018-09-30 20:52:03 -0700

    Created human class and made player a parent class

---
+++ Jajanken/human.cpp
@@ -0,0 +1,24 @@
+#include "human.hpp"
+
+human::human()
+{
+
+}
+
+void human::selectMove(Rps_Moves move)
+{ 
+    switch(move)
+    {
+        case Rps_Moves::ROCK:
+            setMove(Rps_Moves::ROCK);
+            break;
+        case Rps_Moves::PAPER:
+            setMove(Rps_Moves::PAPER);
+            break;
+        case Rps_Moves::SCISSORS:
+            setMove(Rps_Moves::SCISSORS);
+            break;
+        default:
+            break;
+    }
+}
---
+++ Jajanken/human.hpp
@@ -0,0 +1,13 @@
+#ifndef HUMAN_H_
+#define HUMAN_H_
+
+#include "Player.hpp"
+
+class human:public Player
+{
+public:
+    human();
+    void selectMove(Rps_Moves);
+};
+
+#endif
