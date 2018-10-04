059dfaf96088bf7d1272a30956e3d56fdc683dc5
Alejandro Perez <alex2721ap@gmail.com>
2018-10-03 19:04:14 -0700

    Edited selectMove() for human.cpp

--- Jajanken/human.cpp
+++ Jajanken/human.cpp
@@ -2,20 +2,4 @@
 
 human::human(){}
 
-void human::selectMove(Rps_Moves move)
-{ 
-    switch(move)
-    {
-        case Rps_Moves::ROCK:
-            setMove(Rps_Moves::ROCK);
-            break;
-        case Rps_Moves::PAPER:
-            setMove(Rps_Moves::PAPER);
-            break;
-        case Rps_Moves::SCISSORS:
-            setMove(Rps_Moves::SCISSORS);
-            break;
-        default:
-            break;
-    }
-}
+void human::selectMove(Rps_Moves move) {setMove(move);}
