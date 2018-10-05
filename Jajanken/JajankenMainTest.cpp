4e3e076ce6639dbcb93b2b43d401ae805c673def
NicoleB <Triforce@Nicoles-MacBook-Pro.local>
2018-10-05 15:21:10 -0700

    Started filestream

--- Jajanken/JajankenMainTest.cpp
+++ Jajanken/JajankenMainTest.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <fstream>
 #include "Player.hpp"
 #include "Computer.hpp"
 #include "GameResults.hpp"
@@ -6,75 +8,83 @@
 
 int main()
 {
-	Player player;
-	Computer computer;
-	RockPaperScissor game;
-	char choice;
-
-	std::cout << "Would you like to see the rules? (Y) (N)" << std::endl;
-	std::cin >> choice;
-
-	if(toupper(choice) == 'Y')
-	{
-		game.displayRules();
-	}
-
-	std::cout<<"Play Rock Paper Scissors? (Y) (N)" << std::endl;
-	std::cin >> choice;
-
-	if(toupper(choice) =='Y')
-	{
-		int i = 0;
-		while(i < 20)
-		{
-			std::cout<< '\n';
-			game.showGame();
-			std::cout << "Select A B or C" << std::endl;
-			std::cout << "A) Rock " << std::endl;
-			std::cout << "B) Paper " << std::endl;
-			std::cout << "C) Scissors " << std::endl;
-			std::cin >> choice;
-
-
-			switch(toupper(choice))
-			{
-			case 'A':
-				player.selectMove(moves::ROCK);
-				break;
-
-			case 'B':
-				player.selectMove(moves::PAPER);
-				break;
-
-			case 'C':
-				player.selectMove(moves::SCISSORS);
-				break;
-
-			default: std::cout << "Not a Valid Option" <<std::endl;
-			break;
-
-			}
-			computer.selectMove();
-			computer.displaySelectedMove();
-			player.displaySelectedMove();
-			std::cout<<std::endl;
-			game.compareMoves(player.getMove(), computer.getMove());
-
-			std::cout<<'\n';
-
-			i++;
-		}
-
-		game.showEndGame();
-
-
-	}
-	else
-	{
-		std::cout<<"Goodbye" << std::endl;
-		return 0;
-	}
-
-	return 0;
-
+    Player player;
+    Computer computer;
+    RockPaperScissor game;
+    char choice;
+    
+    std::ofstream file;
+    file.open("data.txt");
+    
+    std::cout << "Would you like to see the rules? (Y) (N)" << std::endl;
+    std::cin >> choice;
+    
+    if(toupper(choice) == 'Y')
+    {
+        game.displayRules();
+    }
+    
+    std::cout<<"Play Rock Paper Scissors? (Y) (N)" << std::endl;
+    std::cin >> choice;
+    
+    if(toupper(choice) =='Y')
+    {
+        int i = 0;
+        while(i < 20)
+        {
+            std::cout<< '\n';
+            game.showGame();
+            std::cout << "Select A B or C" << std::endl;
+            std::cout << "A) Rock " << std::endl;
+            std::cout << "B) Paper " << std::endl;
+            std::cout << "C) Scissors " << std::endl;
+            std::cin >> choice;
+            
+            //file << choice;
+            
+            
+            switch(toupper(choice))
+            {
+                case 'A':
+                    player.setMove(moves::ROCK);
+                    break;
+                    
+                case 'B':
+                    player.setMove(moves::PAPER);
+                    break;
+                    
+                case 'C':
+                    player.setMove(moves::SCISSORS);
+                    break;
+                    
+                default: std::cout << "Not a Valid Option" <<std::endl;
+                    break;
+                    
+            }
+            computer.generateMove();
+            //computer.displaySelectedMove();
+            player.displaySelectedMove();
+            std::cout<<std::endl;
+            //game.compareMoves(player.getMove(), computer.getMove());
+            
+            std::cout<<'\n';
+            
+            i++;
+        }
+        
+        game.showEndGame();
+        
+        
+    }
+    else
+    {
+        std::cout<<"Goodbye" << std::endl;
+        return 0;
+    }
+    
+    file.close();
+    
+    return 0;
+    
 }
+
