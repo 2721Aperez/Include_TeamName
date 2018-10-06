e9c4e449a055fe9b4d599aa62602451aee2ccd5d
jStokesSJSU <43454674+jStokesSJSU@users.noreply.github.com>
2018-10-05 22:48:40 -0700

    Delete ChooserFactory.cpp
    wrong directory

--- ChooserFactory.cpp
+++
@@ -1,9 +0,0 @@
-#include "ChooserFactory.hpp"
-#include "ChooserTypes.hpp"
-
-ChooserInterface *ChooserFactory::make_chooser(std::string choice)
-{
-	if (choice == ("random")) { return new RandomChooser(); }
-	else if (choice == "smart") { return new SmartChooser(); }
-	else { return new RandomChooser(); }
-}
